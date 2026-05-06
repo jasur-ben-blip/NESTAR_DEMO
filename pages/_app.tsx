edcff79ca55560620a2e78a9c6d0a4b88fe9086a
Jasur(BEN) <abdujambulovj@gmail.com>
2026-05-06 23:08:06 +0900

    feat: develop apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,25 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "@/scss/MaterialTheme";
 import { useState } from "react";
+
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
+
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  //@ts-ignore
+  // @ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
 
-  // socket.io , Redux, global integration we can do
-
+  //Socket.io, Redux, Mui ...
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,31 @@ import Advertisement from "@/libs/components/homePage/Advertisement";
 import TopProperties from "@/libs/components/homePage/TopProperties";
 import TopAgents from "@/libs/components/homePage/TopAgents";
 import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { useQuery } from "@apollo/client";
+import { GET_PROPERTIES } from "@/apollo/user/query";
 
 const Home: NextPage = () => {
   const device = useDeviceDetect();
+
+  const {
+    loading: getPropertiesLoading,
+    data: getPropertiesData,
+    error: getPropertiesError,
+    refetch: getPropertiesRefetch,
+  } = useQuery(GET_PROPERTIES, {
+    fetchPolicy: "network-only",
+    variables: {
+      input: {
+        page: 1,
+        limit: 5,
+        sort: "propertyRank",
+        direction: "DESC",
+        search: {},
+      },
+    },
+  });
+
+  console.log("getPropertiesData => ", getPropertiesData);
 
   if (device === "mobile") {
     return <Stack>Home page mobile</Stack>;
